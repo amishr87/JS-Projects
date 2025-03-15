2b7893a572765f249d5b9579b0258a5bcaed3e3f
Anand Krishna Mishra <160732081+amishr87@users.noreply.github.com>
2025-03-15 11:28:35 -0700

    Update chaiaurcode.js

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,22 @@
-console.log('Project 5');
+const insert = document.getElementById('insert');
+
+//writing e.key would have been sufficient but this is to ensure if I press space bar, "space" in printer instead of a blank
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>       
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>    
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
